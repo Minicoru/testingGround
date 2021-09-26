aa8ff917b67493614775331be798776ca3b62fc4
Mia Ruiz (Desktop) <miarg49@gmail.com>
2021-09-26 20:02:17 -0300

    sincro desktop

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,7 +7,9 @@ sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
 
 			var url = '/sap/opu/odata/SAP/<service>/';
 
-			oModel = new sap.ui.model.odata.ODataModel();
+			oModel = new sap.ui.model.json.JSONModel();
+			oModel.setData([]);
+			// oModel = new sap.ui.model.odata.ODataModel();
 			// url, true
 
 			//* Create Items Aggregation for Table
